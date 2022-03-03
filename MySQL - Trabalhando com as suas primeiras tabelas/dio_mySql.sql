1e0c22c90df9207eb09b59710f1f0395926d109f
gellwanger <gustavojce@gmail.com>
2022-03-03 14:49:00 -0300

    Remove usuario do banco de dados pessoa

--- MySQL - Trabalhando com as suas primeiras tabelas/dio_mySql.sql
+++ MySQL - Trabalhando com as suas primeiras tabelas/dio_mySql.sql
@@ -3,14 +3,14 @@ CREATE DATABASE dio_mysql;
 USE dio_mysql;
 
 CREATE TABLE pessoas (
-	nome VARCHAR(20),
+	nome VARCHAR(30),
     nascimento DATE,
     PRIMARY KEY(nome)
 );
 
 CREATE TABLE pessoa (
 	id INT AUTO_INCREMENT NOT NULL,
-    nome VARCHAR(10) NOT NULL,
+    nome VARCHAR(30) NOT NULL,
     nascimento DATE,
     PRIMARY KEY(id)
 );
@@ -21,8 +21,16 @@ SELECT * FROM pessoas;
 
 INSERT INTO pessoas (nome, nascimento) VALUES ('Franciele', '1989-12-08');
 
-SELECT * FROM pessoas;
+SELECT * FROM pessoa;
 
 INSERT INTO pessoa (nome, nascimento) VALUES ('Gustavo', '1992-02-10');
+
+SELECT * FROM pessoa;
+
+UPDATE pessoa SET nome='Gustavo Cecchin';
+
+SELECT * FROM pessoa;
+
+DELETE FROM pessoa WHERE id=1;
 
 SELECT * FROM pessoa;
