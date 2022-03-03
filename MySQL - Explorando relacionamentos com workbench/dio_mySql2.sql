2f626a00af882e80ec9270820b1eec3da2ae10c4
gellwanger <gustavojce@gmail.com>
2022-03-03 16:01:57 -0300

    Adiciona dados as tabelas e faz relaçoes com elas

--- MySQL - Explorando relacionamentos com workbench/dio_mySql2.sql
+++ MySQL - Explorando relacionamentos com workbench/dio_mySql2.sql
@@ -4,7 +4,7 @@ USE dio_mySql2;
 
 CREATE TABLE videos (
 	id_video INT AUTO_INCREMENT,
-    author VARCHAR(30) NOT NULL,
+    author VARCHAR(30),
     title VARCHAR(30) NOT NULL,
     likes INT,
     dislikes INT,
@@ -30,14 +30,40 @@ CREATE TABLE author (
     PRIMARY KEY(id_author)
 );
 
-	SELECT * FROM author;
+SELECT * FROM author ORDER BY id_author;
 
-	INSERT INTO author (author_name, born) VALUES ('Maria', '1992-08-19');
+INSERT INTO author (author_name, born) VALUES ('Maria', '1992-08-19');
 
-	INSERT INTO author (author_name, born) VALUES ('João', '1990-03-12');
+INSERT INTO author (author_name, born) VALUES ('João', '1990-03-12');
 
-	INSERT INTO author (author_name, born) VALUES ('Pedro', '1993-12-15');
+INSERT INTO author (author_name, born) VALUES ('Maria', '1992-08-19');
 
-	INSERT INTO author (author_name, born) VALUES ('Alucard', '1995-03-29');
+INSERT INTO author (author_name, born) VALUES ('Maria', '1992-08-19');
 
-	INSERT INTO author (author_name, born) VALUES ('Joana', '1989-05-02');
+INSERT INTO author (author_name, born) VALUES ('João', '1990-03-12');
+
+UPDATE videos SET author='';
+
+SELECT * FROM videos;
+
+ALTER TABLE videos
+DROP COLUMN author;
+
+SELECT * FROM videos;
+
+ALTER TABLE videos
+ADD author INT NOT NULL;
+
+SELECT * FROM videos;
+
+UPDATE videos SET author=1 WHERE id_video=1;
+
+UPDATE videos SET author=1 WHERE id_video=3;
+
+UPDATE videos SET author=1 WHERE id_video=4;
+
+UPDATE videos SET author=2 WHERE id_video=2;
+
+UPDATE videos SET author=2 WHERE id_video=5;
+
+SELECT * FROM videos;
